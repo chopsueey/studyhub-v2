8b03821ab90cd955137a9e4291c1b6d9df47c529
chopsueey <meyer-marius@live.de>
2025-07-12 16:21:29 +0200

    api: using the lite model with lower latency

--- app/backend/functions/promptAI.ts
+++ app/backend/functions/promptAI.ts
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import type { QuillFormat } from "../models/Note";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite-preview-06-17" });
 
 export async function promptAI(option: number, noteContent: QuillFormat[]) {
   const content = noteContent.map((item: QuillFormat) => item.insert).join("");
